6c384222af3be00b245616d9f1603db7386733ef
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-06-29 13:09:10 +0300

    feat: add new types

---
+++ migration-to-ts/src/components/types/types.ts
@@ -0,0 +1,36 @@
+export interface Sources {
+	status: string;
+	sources: Source[];
+}
+
+export interface Source {
+	id: string;
+	name: string;
+	description: string;
+	url: string;
+	category: string;
+	language: string;
+	country: string;
+}
+
+export interface Everything {
+	status: string;
+	totalResults: number;
+	articles: Article[];
+}
+
+export interface Article {
+	source: ArticleSource;
+	author: string;
+	title: string;
+	description: string;
+	url: string;
+	urlToImage: string;
+	publishedAt: string;
+	content: string;
+}
+
+type ArticleSource = {
+	id: string;
+	name: string;
+}
