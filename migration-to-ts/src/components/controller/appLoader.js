5654db9dd2f094ed7d09aa2dccab4dfa84cd0908
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-06-29 11:47:41 +0300

    feat: add news api key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'c8457a68878146cc9ce1a2b430945902', // получите свой ключ https://newsapi.org/
         });
     }
 }
