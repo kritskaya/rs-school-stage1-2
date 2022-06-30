7fd16ad2a3f131b96268102940d7d172c7b255d6
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-06-30 12:59:22 +0300

    feat: add types to appView

--- migration-to-ts/src/components/view/appView.ts
+++ migration-to-ts/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { NewsData, SourcesData } from '../types/types';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: NewsData) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesData) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
