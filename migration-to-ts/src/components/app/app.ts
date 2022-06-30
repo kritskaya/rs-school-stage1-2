b90e0bac2dfc05cbb423e4a7826393593bed5fae
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-06-30 15:55:34 +0300

    feat: add types to app

---
+++ migration-to-ts/src/components/app/app.ts
@@ -0,0 +1,23 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { NewsData, SourcesData } from '../types/types';
+
+class App {
+    private controller: AppController;
+    private view: AppView;
+
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        document
+            .querySelector('.sources')?.addEventListener('click', (e) => 
+                this.controller.getNews(e, (data?: NewsData) => this.view.drawNews(data as NewsData))
+            );
+        this.controller.getSources((data?: SourcesData) => this.view.drawSources(data as SourcesData));
+    }
+}
+
+export default App;
