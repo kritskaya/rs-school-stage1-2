bfb61e587e2e00e6e5317f3bea77aeb774361b8a
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-07-13 18:28:57 +0300

    feat: add filter model

---
+++ online-store/src/scripts/model/filter.model.ts
@@ -0,0 +1,27 @@
+enum SizeFilterType {
+	Size60x120 = "60x120",
+	Size80x200 = "80x200",
+	Size90x120 = "90x200",
+	Size160x200 = "160x200",
+	Size180x120 = "200x200",
+}
+
+export class Filter<T> {
+	constructor(
+		private title: string,
+		private field: string,
+		private value: T
+	) {}
+
+	public getTitle(): string {
+		return this.title;
+	}
+
+	public getField(): string {
+		return this.field;
+	}
+
+	public getValue(): T {
+		return this.value;
+	}
+}
