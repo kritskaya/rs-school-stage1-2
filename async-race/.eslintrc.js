08d9ed67819a9829ebf39cc9bf1d470c11d2702b
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-07-27 20:33:32 +0300

    init: start task async race

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb-typescript"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": 2
+    }
+}
