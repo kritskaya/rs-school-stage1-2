a5f5571503f9eac800ef0afb487fe1c96b0eb487
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-07-27 22:12:04 +0300

    feat: add engine type

--- async-race/src/scripts/model/car.model.ts
+++ async-race/src/scripts/model/car.model.ts
@@ -4,10 +4,16 @@ export interface ICar {
 	color: string;
 }
 
+export interface IEngine {
+	id: number,
+	status: 'started' | 'stopped' | 'drive';
+}
+
 export class Car {
 	private id: number;
 	private name: string;
 	private color: string;
+	private engine: Omit<IEngine, 'id'>;
 
 	constructor (id: number, name: string, color: string) {
 		this.id = id;
@@ -29,5 +35,13 @@ export class Car {
 
 	public setColor(color: string): void {
 		this.color = color;
+	}
+
+	public getEngine(): Omit<IEngine, 'id'> {
+		return this.engine;
+	}
+
+	public setEngine(engine: Omit<IEngine, 'id'>): void {
+		this.engine = engine;
 	}
 }
